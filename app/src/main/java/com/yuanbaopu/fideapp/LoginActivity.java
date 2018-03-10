1b3206405178996b4ed91452c97be3d99182dfba
xiongkb <xiongkb@asto-inc.com>
2018-03-10 16:55:40 +0800

    project update

---
+++ app/src/main/java/com/yuanbaopu/fideapp/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.yuanbaopu.fideapp;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    //登录
+    void attemptLogin() {
+
+    }
+}
+
