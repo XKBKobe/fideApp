b67a257a71af8bd0c89e4473c91fa309c711feb9
xiongkb <xiongkb@asto-inc.com>
2018-03-11 22:11:09 +0800

    update okhttp

--- app/src/main/java/com/yuanbaopu/fideapp/LoginActivity.java
+++ app/src/main/java/com/yuanbaopu/fideapp/LoginActivity.java
@@ -5,11 +5,21 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Headers;
+
+
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity  {
+public class LoginActivity extends AppCompatActivity {
+    private final OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +28,34 @@ public class LoginActivity extends AppCompatActivity  {
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 attemptLogin();
             }
         });
     }
 
     //登录
-    void attemptLogin() {
+    public void attemptLogin()  {
+        System.out.println(11111111);
+        Request request = new Request.Builder()
+                .url("https://fidebsz.yuanbaopu.com/api/")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            Headers responseHeaders = response.headers();
+            for (int i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+
+            System.out.println(response.body().string());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
