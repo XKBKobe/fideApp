98744271bd2962a59a2cb1d5c911f0f840b187da
xiongkb <xiongkb@asto-inc.com>
2018-03-13 13:55:32 +0800

    update code

--- app/src/main/java/com/yuanbaopu/fideapp/KbHttp.java
+++ app/src/main/java/com/yuanbaopu/fideapp/KbHttp.java
@@ -7,30 +7,30 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.RequestBody;
 import okhttp3.FormBody;
-import okhttp3.Call;
 
 public class KbHttp {
     OkHttpClient client = new OkHttpClient();
 
-    String run(String url) throws IOException {
-        System.out.println(121212);
+    String Post(String url) throws IOException {
         RequestBody body = new FormBody.Builder()
-                .add("login", "18055417483")
-                .add("pass", "07809709").build();
+                .add("loginName", "18055417483")
+                .add("password", "18055417483")
+                .add("sysSource", "2").build();
         Request request = new Request.Builder()
                 .url(url)
                 .post(body)
                 .build();
-        Call call = client.newCall(request);
-        Response response = call.execute();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
         System.out.println(response);
         return  response.body().string();
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(1212121212);
-        KbHttp example = new KbHttp();
-        String response = example.run("https://fidebszltest.yuanbaopu.com/api/");
-        System.out.println(response);
+//        System.out.println(1212121212);
+//        KbHttp example = new KbHttp();
+//        String response = example.run("https://fidebsz.yuanbaopu.com/api/");
+//        System.out.println(response);
     }
 }
--- app/src/main/java/com/yuanbaopu/fideapp/LoginActivity.java
+++ app/src/main/java/com/yuanbaopu/fideapp/LoginActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.FormBody;
@@ -13,6 +15,11 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
@@ -40,7 +47,6 @@ public class LoginActivity extends AppCompatActivity {
 
     //登录
     public void attemptLogin()  {
-
         new Thread(networkTask).start();
     }
 
@@ -49,9 +55,8 @@ public class LoginActivity extends AppCompatActivity {
         KbHttp http = new KbHttp();
         @Override
         public void run() {
-//            https://fidebszltest.yuanbaopu.com/api/
             try{
-                http.run("https://fidebszltest.yuanbaopu.com/api/");
+                http.Post("http://inet-local-k8s-master.yuanbaopu.com:26030");
             }catch (Exception e) {
                 e.printStackTrace();
             };
